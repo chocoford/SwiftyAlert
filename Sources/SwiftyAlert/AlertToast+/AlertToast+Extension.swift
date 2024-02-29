991af27e31a97b0a98820ab4b655b79933c85710
Chocoford <chocoford@icloud.com>
2024-02-29 13:38:12 +0800

    Update AlertToast+Extension.swift

--- Sources/SwiftyAlert/AlertToast+/AlertToast+Extension.swift
+++ Sources/SwiftyAlert/AlertToast+/AlertToast+Extension.swift
@@ -9,7 +9,6 @@ import Foundation
 import SwiftUI
 #if canImport(AlertToast)
 import AlertToast
-#endif
 
 extension AlertToast {
     public init(
@@ -28,3 +27,22 @@ extension AlertToast {
         }
     }
 }
+#else
+extension FakeAlertToast {
+    public init(
+        error: Error,
+        displayMode: FakeAlertToast.DisplayMode = .hud,
+        type: FakeAlertToast.AlertType = .error(.red)
+    ) {
+        if let error = error as? LocalizedError {
+            self = .init(
+                displayMode: displayMode, type: type,
+                title: error.errorDescription, subTitle: error.failureReason
+            )
+
+        } else {
+            self = .init(displayMode: displayMode, type: type, title: error.localizedDescription)
+        }
+    }
+}
+#endif
