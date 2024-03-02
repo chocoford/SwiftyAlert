91655de86a2057263b6e2983b91f6268ce875990
Chocoford <chocoford@icloud.com>
2024-03-03 03:25:21 +0800

    try to fix undefined symbol bug

--- Sources/SwiftyAlert/AlertToast+/AlertToast+Extension.swift
+++ Sources/SwiftyAlert/AlertToast+/AlertToast+Extension.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 #if canImport(AlertToast)
 import AlertToast
+#endif
 
 extension AlertToast {
     public init(
@@ -27,22 +28,22 @@ extension AlertToast {
         }
     }
 }
-#else
-extension FakeAlertToast {
-    public init(
-        error: Error,
-        displayMode: FakeAlertToast.DisplayMode = .hud,
-        type: FakeAlertToast.AlertType = .error(.red)
-    ) {
-        if let error = error as? LocalizedError {
-            self = .init(
-                displayMode: displayMode, type: type,
-                title: error.errorDescription, subTitle: error.failureReason
-            )
-
-        } else {
-            self = .init(displayMode: displayMode, type: type, title: error.localizedDescription)
-        }
-    }
-}
-#endif
+//#else
+//extension FakeAlertToast {
+//    public init(
+//        error: Error,
+//        displayMode: FakeAlertToast.DisplayMode = .hud,
+//        type: FakeAlertToast.AlertType = .error(.red)
+//    ) {
+//        if let error = error as? LocalizedError {
+//            self = .init(
+//                displayMode: displayMode, type: type,
+//                title: error.errorDescription, subTitle: error.failureReason
+//            )
+//
+//        } else {
+//            self = .init(displayMode: displayMode, type: type, title: error.localizedDescription)
+//        }
+//    }
+//}
+//#endif
