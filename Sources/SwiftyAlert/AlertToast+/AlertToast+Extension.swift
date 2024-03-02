0ac2c2e73407c757fdea8baa2b41c5df13747320
Chocoford <chocoford@icloud.com>
2024-03-03 03:47:23 +0800

    try to fix undefined symbol bug

--- Sources/SwiftyAlert/AlertToast+/AlertToast+Extension.swift
+++ Sources/SwiftyAlert/AlertToast+/AlertToast+Extension.swift
@@ -9,13 +9,13 @@ import Foundation
 import SwiftUI
 #if canImport(AlertToast)
 import AlertToast
-//#endif
+#endif
 
 extension AlertToast {
     public init(
         error: Error,
-        displayMode: AlertToast.DisplayMode = AlertToast.DisplayMode.hud,
-        type: AlertToast.AlertType = AlertToast.AlertType.error(.red)
+        displayMode: DisplayMode = DisplayMode.hud,
+        type: AlertType = AlertType.error(.red)
     ) {
         if let error = error as? LocalizedError {
             self = .init(
@@ -34,28 +34,22 @@ extension AlertToast {
         }
     }
 }
-#else
-extension FakeAlertToast {
-    public init(
-        error: Error,
-        displayMode: FakeAlertToast.DisplayMode = FakeAlertToast.DisplayMode.hud,
-        type: FakeAlertToast.AlertType = FakeAlertToast.AlertType.error(.red)
-    ) {
-        if let error = error as? LocalizedError {
-            self = .init(
-                displayMode: displayMode,
-                type: type,
-                title: error.errorDescription,
-                subTitle: error.failureReason
-            )
-
-        } else {
-            self = .init(
-                displayMode: displayMode,
-                type: type,
-                title: error.localizedDescription
-            )
-        }
-    }
-}
-#endif
+//#else
+//extension FakeAlertToast {
+//    public init(
+//        error: Error,
+//        displayMode: FakeAlertToast.DisplayMode = .hud,
+//        type: FakeAlertToast.AlertType = .error(.red)
+//    ) {
+//        if let error = error as? LocalizedError {
+//            self = .init(
+//                displayMode: displayMode, type: type,
+//                title: error.errorDescription, subTitle: error.failureReason
+//            )
+//
+//        } else {
+//            self = .init(displayMode: displayMode, type: type, title: error.localizedDescription)
+//        }
+//    }
+//}
+//#endif
